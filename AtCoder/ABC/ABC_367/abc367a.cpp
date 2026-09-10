d90dc2fe00c09d41ce89985f7e892e7c7c388667
Lakshya Poonia <lppoonia2007@gmail.com>
2026-09-10 14:19:15 +0530

    start

---
+++ AtCoder/ABC/ABC_367/abc367a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define inarr(a, n) for (int _i = 0; _i < (n); _i++) cin >> (a)[_i];
+#define invec(v) for (auto &_x : (v)) cin >> _x;
+
+using namespace std;
+
+bool multipleTests = false;
+
+void solve() {
+    ll a, b, c; cin >> a >> b >> c;
+    while(b != c) {
+        if(b == a) {
+            cout << "No" << endl;
+            return;
+        }
+        b = (b + 1) % 24;
+    }
+    
+    cout << "Yes" << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    if (multipleTests)
+        cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
